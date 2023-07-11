988014173d7b843c7cee2b10b65c2f7214c40187
pedrogas <srpj4321@gmail.com>
2023-07-11 12:50:49 -0300

    shop/items

--- src/pages/home/Shop.jsx
+++ src/pages/home/Shop.jsx
@@ -1,12 +1,48 @@
+import { LiaAngleRightSolid } from 'react-icons/lia'
+import Items from './Items';
+
+
 export default function Shop() {
-    return (
-      <section className="shop">
-        <div className="designed-by-you">
-            <h2>Designed by you</h2>
-            <h3>Your Style</h3>
-            <p>Express your unique style with customizable lenses and frames. Make a statement that's exclusively you. Discover endless possibilities.</p>
-            <button className="shop-now"></button>
+
+  function Price({name, price, dotId, itemId}){
+    return(
+      <>
+        <div className="dot" id={dotId}>
+          <div className="mid-dot"></div>
         </div>
-      </section>
-    );
+        <div className="item-info" id={itemId}>
+          <span className="new">New</span>
+          <span className="item-name">{name}</span>
+          <span className="item-price"><span className="dollar">$</span>{(price).toFixed(2)}</span>
+          <LiaAngleRightSolid className='arrow'/>
+        </div>
+      </>
+    )
   }
+
+  return (
+    <section className="shop">
+      <div className="designed-by-you">
+          <h2>Designed by you</h2>
+          <h3>Your Style</h3>
+          <p>Express your unique style with customizable lenses and frames. Make a statement that's exclusively you. Discover endless possibilities.</p>
+          <button className="shop-now-button">Shop now</button>
+      </div>
+      <div className="shop-images">
+        {Items.map((item, index)=>{
+          return(
+            <picture className="item" key={index+1}>
+              <img src={`/images/style-img-${index+1}-edit.jpg`} alt="" />
+              <Price
+                name={item.name}
+                price={item.price}
+                itemId={`item-${index+1}`}
+                dotId={`dot-${index+1}`}
+              />
+            </picture>
+          )
+        })}
+      </div>
+    </section>
+  );
+}
